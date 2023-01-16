e05a12291126d8e8555be443b71c74698e218162
Glitch (ifttt-hello-world) <none>
2023-01-16 19:58:33 +0000

    ☕️👰 Checkpoint ./middleware.js:52991551/4995

--- middleware.js
+++ middleware.js
@@ -1,16 +1,18 @@
 const IFTTT_SERVICE_KEY = process.env.IFTTT_SERVICE_KEY;
 
 module.exports = {
-  
   serviceKeyCheck: function (req, res, next) {
-
     const key = req.get("IFTTT-Service-Key");
-    
+
     if (key !== IFTTT_SERVICE_KEY) {
-      res.status(401).send();
+      res.status(401).json({
+        "errors": [{
+          "message": "Channel/Service key is not correct"
+        }]
+      });
+      return;
     }
 
     next();
-    
-  }
+  },
 };
