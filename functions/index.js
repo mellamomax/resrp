878278689da535d881c0931230f768c47b97da60
mellamomax <122839345+mellamomax@users.noreply.github.com>
2023-01-19 00:10:54 +0100

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -2,13 +2,19 @@ const fastify = require('fastify');
 const app = fastify();
 
 app.get('/', (req, res) => {
-    res.send({ message: 'Hello World' });
+res.send({ message: 'Hello World' });
 });
 
 app.get('/about', (req, res) => {
-    res.send({ message: 'About Page' });
+res.send({ message: 'About Page' });
 });
 
 exports.handler = (event, context, callback) => {
-    app.listen(3000, () => { console.log('Server started on port 3000'); });
+app.listen(3000, (err) => {
+if (err) {
+callback(err);
+}
+const handler = app.server.createHandler();
+handler(event, context, callback);
+});
 };
