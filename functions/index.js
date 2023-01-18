d1e7ba46a322d580d1720c06d3a04763890a7054
mellamomax <122839345+mellamomax@users.noreply.github.com>
2023-01-19 00:01:32 +0100

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -10,8 +10,5 @@ app.get('/about', (req, res) => {
 });
 
 exports.handler = (event, context, callback) => {
-    app.ready(() => {
-        const handler = app.server.createHandler();
-        handler(event, context, callback);
-    });
+    app.listen(3000, () => { console.log('Server started on port 3000'); });
 };
