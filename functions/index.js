664de7712697e81e835fd7194334d25bbcd96057
mellamomax <122839345+mellamomax@users.noreply.github.com>
2023-01-18 23:08:09 +0100

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,11 @@ app.get('/', (req, res) => {
     res.send({ message: 'Hello World' });
 });
 
+app.get('/about', (req, res) => {
+    res.send({ message: 'About Page' });
+});
+
+
 module.exports.handler = (event, context, callback) => {
     const server = awsServerlessFastify(app);
     server(event, context, callback);
