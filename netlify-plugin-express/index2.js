4b64450dd93a17f10430332564452de8b98d6195
mellamomax <122839345+mellamomax@users.noreply.github.com>
2023-01-18 22:03:30 +0100

    Update index2.js

--- netlify-plugin-express/index2.js
+++ netlify-plugin-express/index2.js
@@ -1,1 +1,18 @@
-asd
+const express = require('express')
+const serverless = require('aws-serverless-fastify')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/test', (req, res) => {
+  res.send('Test Route!')
+})
+
+const handler = serverless(app)
+
+module.exports = {
+  handler
+}
