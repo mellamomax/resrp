68ee431dbd04726421b4ef794e3b5bc59335bb22
mellamomax <122839345+mellamomax@users.noreply.github.com>
2023-01-18 23:40:28 +0100

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,6 @@ const fastify = require('fastify');
 const app = fastify();
 const awsServerlessFastify = require('aws-serverless-fastify');
 
-
 app.get('/', (req, res) => {
     res.send({ message: 'Hello World' });
 });
@@ -11,8 +10,4 @@ app.get('/about', (req, res) => {
     res.send({ message: 'About Page' });
 });
 
-
-module.exports.handler = (event, context, callback) => {
-    app.listen(3000, () => { console.log('Server started on port 3000'); });
-};
-
+module.exports.handler = awsServerlessFastify(app);
